ec9fa1fc81b0dfb8a482b65d293588a7e1d15d67
Roman Zykov <87817259+RomaZykov@users.noreply.github.com>
2024-05-04 19:17:10 +0300

    Complete 7 task
    Complete 7 task

---
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/PreferencesKeys.kt
@@ -0,0 +1,7 @@
+package ru.easycode.zerotoheroandroidtdd
+
+import androidx.datastore.preferences.core.stringPreferencesKey
+
+object PreferencesKeys {
+    val CURRENT_TITLE = stringPreferencesKey("current_title")
+}
