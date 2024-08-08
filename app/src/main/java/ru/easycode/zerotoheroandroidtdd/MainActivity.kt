20251836fe4bce3a86b7c89989bc892608f6f70e
Roman Zykov <87817259+RomaZykov@users.noreply.github.com>
2024-08-08 20:01:44 +0300

    Solve 22 task

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,52 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Button
+import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.TextView
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var contentLayout: LinearLayout
+    private lateinit var editText: TextInputEditText
+    private lateinit var actionButton: Button
+
+    private val contentList = mutableListOf<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        contentLayout = findViewById(R.id.contentLayout)
+        editText = findViewById(R.id.inputEditText)
+        actionButton = findViewById(R.id.actionButton)
+
+        actionButton.setOnClickListener {
+            val newText = TextView(this)
+            newText.text = editText.text.toString()
+            contentList.add(newText.text.toString())
+            contentLayout.addView(newText)
+            editText.text?.clear()
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putStringArray("contentList", contentList.toTypedArray())
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val contentTextView = savedInstanceState.getStringArray("contentList")
+        if (contentTextView != null) {
+            contentTextView.forEach {
+                val newText = TextView(this)
+                newText.text = it.toString()
+                contentLayout.addView(newText)
+            }
+        }
     }
 }
