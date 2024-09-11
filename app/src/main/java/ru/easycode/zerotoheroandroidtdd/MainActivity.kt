bb03218f7d947fe81e6175b485715a705ea2177e
Roman Zykov <87817259+RomaZykov@users.noreply.github.com>
2024-09-11 12:47:12 +0300

    Solve 32 task

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -3,9 +3,16 @@ package ru.easycode.zerotoheroandroidtdd
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import ru.easycode.zerotoheroandroidtdd.ui.theme.ZeroToHeroAndroidTDDTheme
 
@@ -18,9 +25,21 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    //todo
+                    VisibleTextWithButton()
                 }
             }
         }
     }
+}
+
+@Composable
+fun VisibleTextWithButton() {
+    var visible by remember { mutableStateOf(false) }
+    Text(
+        modifier = Modifier.clickable {
+            visible = true
+        }, text = if (visible) {
+            "That's right!"
+        } else "Click me!"
+    )
 }
